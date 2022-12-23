5598e2319184a6050cd00223e392d2577d4b9c17
sahilkakwani9 <sahkakwani@gmail.com>
2022-12-23 20:18:35 +0530

    fixes

--- src/utils/getNFT.js
+++ src/utils/getNFT.js
@@ -4,8 +4,8 @@ import { CONTRACT_ADDRESS, ABI } from "./constants";
 
 export default async function getNFTs(tokenId) {
     try {
-        const { ethereum } = window;
-        if (window) {
+        if (typeof window !== 'undefined'){
+            const { ethereum } = window;
             const provider = new ethers.providers.Web3Provider(ethereum, "any");
             const signer = provider.getSigner();
             const contract = new ethers.Contract(CONTRACT_ADDRESS, ABI, signer);
